a7ddb0808c33570b01b09353f6719e96b11e8ab2
kim0chan <39790536+kim0chan@users.noreply.github.com>
2022-11-06 12:44:13 +0900

    Delete config.php

--- config.php
+++
@@ -1,6 +0,0 @@
-﻿﻿<?
-    $host = "localhost";
-    $dbid = "db2018320178";                    // 자신의 DB ID
-    $dbpass = "1999@korea.ac.kr";                  // 자신의 DB Password
-    $dbname = "db2018320178";                  // 해당 프로젝트에서 사용하는 DB명
-?>
